5de5761126451673ac5c49eff64a56b07a3a3a80
Natalie <ziweih1993@gmail.com>
2024-03-24 01:19:21 -0400

    Change setenv

--- ops/container/setenv.sh
+++ ops/container/setenv.sh
@@ -6,7 +6,7 @@ export JAVA_OPTS="$JAVA_OPTS -Ddatabase.dialect=org.hibernate.dialect.PostgreSQL
 export JAVA_OPTS="$JAVA_OPTS -Dsecret.key=ziwei-anton"
 export JAVA_OPTS="$JAVA_OPTS -Dlogging.level.org.springframework=INFO"
 export JAVA_OPTS="$JAVA_OPTS -Dlogging.levelt.com.ascending=TRACE"
-export JAVA_OPTS="$JAVA_OPTS -Ddatabase.user=adminziwei"
+export JAVA_OPTS="$JAVA_OPTS -Ddatabase.user=ziweiadmin"
 export JAVA_OPTS="$JAVA_OPTS -Ddatabase.password=ziwei123!"
 export JAVA_OPTS="$JAVA_OPTS -Ddatabase.url=jdbc:postgresql://${DB_URL}:${DB_PORT}/${DB_NAME}"
 export JAVA_OPTS="$JAVA_OPTS -Dserver.port=8080"
